640d2afbb67ce14cb699c6d187083cb13b53c8c6
Krisztian Fodor <kris.fodor66@gmail.com>
2022-11-04 16:03:07 +0100

    partial upload exercise 3

---
+++ 04.CSharp-OOP/03.InterfacesAndAbstraction/InterfacesAndAbstraction-Exercise/DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ 04.CSharp-OOP/03.InterfacesAndAbstraction/InterfacesAndAbstraction-Exercise/MultipleImplementation/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson,IIdentifiable,IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthdate;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
