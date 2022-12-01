580341e574b499ad3ba2a76acfb398cecb1ba9ce
Krisztian Fodor <kris.fodor66@gmail.com>
2022-12-01 21:44:25 +0100

    final upload exercise 8

--- 04.CSharp-OOP/08.UnitTesting/UnitTesting-Exercise/Skeleton/FightingArena.Tests/ArenaTests.cs
+++ 04.CSharp-OOP/08.UnitTesting/UnitTesting-Exercise/Skeleton/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,111 @@
-﻿namespace FightingArena.Tests
+﻿using System;
+
+namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System.Collections.Generic;
 
     [TestFixture]
     public class ArenaTests
     {
+        private Warrior _warriorOne;
+        private Warrior _warriorTwo;
+        private Arena _arena;
+        private List<Warrior> _warriors;
 
+        [SetUp]
+        public void Setup()
+        {
+            _warriorOne = new Warrior("Dido", 15, 35);
+            _warriorTwo = new Warrior("Toni", 12, 45);
+
+            _warriors = new List<Warrior>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _arena = new Arena();
+        }
+
+        [Test]
+        public void Test_ArenaConstructorInitialisesCollection_ShouldWork()
+        {
+            _arena = new Arena();
+
+            CollectionAssert.AreEqual(_warriors, _arena.Warriors);
+        }
+
+        //Assume Enroll method works!
+        [Test]
+        public void Test_CountPropertyReturnsCollectionCount_ShouldWork()
+        {
+            _arena.Enroll(_warriorOne);
+            _arena.Enroll(_warriorTwo);
+            _warriors = new List<Warrior>() { _warriorOne, _warriorTwo };
+
+            int expectedCount = _warriors.Count;
+            int actualCount = _arena.Count;
+
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [Test]
+        public void Test_EnrollMethodUpdatesTheCollection_ShouldWork()
+        {
+            _arena.Enroll(_warriorOne);
+            _arena.Enroll(_warriorTwo);
+            _warriors = new List<Warrior>() { _warriorOne, _warriorTwo };
+
+            CollectionAssert.AreEqual(_warriors, _arena.Warriors);
+        }
+
+
+        [Test]
+        public void Test_EnrollMethodCannotAddSameWarrior_ShouldThrow()
+        {
+            _arena = new Arena();
+            _arena.Enroll(_warriorOne);
+
+            Assert.Throws<InvalidOperationException>(() => { _arena.Enroll(_warriorOne); },
+                "Warrior is already enrolled for the fights!");
+        }
+
+        [Test]
+        public void Test_FightMethodAttackerVsDefender_ShouldWork()
+        {
+            _arena = new Arena();
+            _arena.Enroll(_warriorOne);
+            _arena.Enroll(_warriorTwo);
+
+            int expectedHPOne = _warriorOne.HP - _warriorTwo.Damage;
+            int expectedHPTwo = _warriorTwo.HP - _warriorOne.Damage;
+
+            _arena.Fight("Dido", "Toni");
+
+            Assert.IsTrue(_warriorOne.HP == expectedHPOne && _warriorTwo.HP == expectedHPTwo);
+        }
+
+        [Test]
+        public void Test_FightMethodAttackerMissingName_ShouldThrow()
+        {
+            _arena = new Arena();
+            _arena.Enroll(_warriorOne);
+            _arena.Enroll(_warriorTwo);
+
+            Assert.Throws<InvalidOperationException>(() => { _arena.Fight("Penka", _warriorTwo.Name); },
+                "There is no fighter with name Penka enrolled for the fights!");
+        }
+
+        [Test]
+        public void Test_FightMethodDefenderMissingName_ShouldThrow()
+        {
+            _arena = new Arena();
+            _arena.Enroll(_warriorOne);
+            _arena.Enroll(_warriorTwo);
+
+            Assert.Throws<InvalidOperationException>(() => { _arena.Fight(_warriorOne.Name, "Penka"); },
+                "There is no fighter with name Penka enrolled for the fights!");
+        }
     }
 }
--- 04.CSharp-OOP/08.UnitTesting/UnitTesting-Exercise/Skeleton/FightingArena.Tests/WarriorTests.cs
+++ 04.CSharp-OOP/08.UnitTesting/UnitTesting-Exercise/Skeleton/FightingArena.Tests/WarriorTests.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace FightingArena.Tests
 {
     using NUnit.Framework;
@@ -5,6 +7,124 @@ namespace FightingArena.Tests
     [TestFixture]
     public class WarriorTests
     {
+        private Warrior _warrior;
+        private string _name;
+        private int _damage;
+        private int _hp;
+        private Warrior _enemy;
 
+        [SetUp]
+        public void Setup()
+        {
+            _name = "Pesho";
+            _damage = 20;
+            _hp = 50;
+            _warrior = new Warrior(_name, _damage, _hp);
+        }
+
+        [Test]
+        public void Test_WarriorConstructor_ShouldWork()
+        {
+            Assert.AreEqual(_name, _warrior.Name);
+            Assert.AreEqual(_damage, _warrior.Damage);
+            Assert.AreEqual(_hp, _warrior.HP);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("                    ")]
+        public void Test_NameProperty_ShouldThrow(string name)
+        {
+            Assert.Throws<ArgumentException>(() => { _warrior = new Warrior(name, _damage, _hp); },
+                "Name should not be empty or whitespace!");
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-100)]
+        public void Test_DamageProperty_ShouldThrow(int damage)
+        {
+            Assert.Throws<ArgumentException>(() => { _warrior = new Warrior(_name, damage, _hp); },
+                "Damage value should be positive!");
+        }
+
+        [TestCase(-1)]
+        [TestCase(-100)]
+        public void Test_HpProperty_ShouldThrow(int hp)
+        {
+            Assert.Throws<ArgumentException>(() => { _warrior = new Warrior(_name, _damage, hp); },
+                "Your HP is too low in order to attack other warriors!");
+        }
+
+        //Constructor is tested and Works! Hence can call values for second Warrior!
+        [Test]
+        public void Test_AttackMethodReducesHpOfWarriorByDamageValue_ShouldWork()
+        {
+            _enemy = new Warrior("Gosho", 10, 40);
+
+            _warrior.Attack(_enemy);
+
+            int expectedHealth = _hp - _enemy.Damage;
+            int actualHealth = _warrior.HP;
+
+            Assert.AreEqual(expectedHealth, actualHealth);
+        }
+
+        [Test]
+        public void Test_AttackMethodReducesHpOfEnemyByDamageValue_ShouldWork()
+        {
+            _enemy = new Warrior("Gosho", 10, 40);
+
+            int expectedHealth = _enemy.HP - _damage;
+
+            _warrior.Attack(_enemy);
+            int actualHealth = _enemy.HP;
+
+            Assert.AreEqual(expectedHealth, actualHealth);
+        }
+
+        [Test]
+        public void Test_AttackMethodReducesHpOfEnemyByDamageValue_KillsEnemy()
+        {
+            _warrior = new Warrior("Pesho", 41, 35);
+            _enemy = new Warrior("Gosho", 10, 40);
+
+            int expectedHealth = 0;
+
+            _warrior.Attack(_enemy);
+            int actualHealth = _enemy.HP;
+
+            Assert.AreEqual(expectedHealth, actualHealth);
+        }
+
+        [Test]
+        public void Test_AttackMethodWarriorCannotAttack_ShouldThrow()
+        {
+            _warrior = new Warrior(_name, _damage, 30);
+            _enemy = new Warrior("Gosho", 10, 40);
+
+            Assert.Throws<InvalidOperationException>(() => { _warrior.Attack(_enemy); },
+                "Your HP is too low in order to attack other warriors!");
+        }
+
+        [Test]
+        public void Test_AttackMethodEnemyCannotAttack_ShouldThrow()
+        {
+            _warrior = new Warrior(_name, _damage, 35);
+            _enemy = new Warrior("Gosho", 10, 30);
+
+            Assert.Throws<InvalidOperationException>(() => { _warrior.Attack(_enemy); },
+                "Enemy HP must be greater than 30 in order to attack him!");
+        }
+
+        [Test]
+        public void Test_AttackMethodWarriorCannotAttackTooWeak_ShouldThrow()
+        {
+            _warrior = new Warrior(_name, _damage, _hp);
+            _enemy = new Warrior("Gosho", 60, 40);
+
+            Assert.Throws<InvalidOperationException>(() => { _warrior.Attack(_enemy); },
+                "You are trying to attack too strong enemy");
+        }
     }
 }
