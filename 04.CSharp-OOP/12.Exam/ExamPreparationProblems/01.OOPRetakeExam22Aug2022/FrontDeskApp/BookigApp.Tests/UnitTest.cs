c75132d2730e4841009716e572297381cf4e80d8
Krisztian Fodor <kris.fodor66@gmail.com>
2022-12-06 21:20:56 +0100

    exam skeleton prep more

---
+++ 04.CSharp-OOP/12.Exam/ExamPreparationProblems/01.OOPRetakeExam22Aug2022/FrontDeskApp/BookigApp.Tests/UnitTest.cs
@@ -0,0 +1,22 @@
+using FrontDeskApp;
+using NUnit.Framework;
+using System;
+
+namespace BookigApp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+   
+    }
+}
