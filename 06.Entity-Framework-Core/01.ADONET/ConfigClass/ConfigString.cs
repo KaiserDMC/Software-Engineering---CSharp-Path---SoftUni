bcba791b7f81f76c648fa494f71ad6dd2a0747de
Krisztian Fodor <kris.fodor66@gmail.com>
2023-02-20 20:54:10 +0100

    pwd

--- 06.Entity-Framework-Core/01.ADONET/ConfigClass/ConfigString.cs
+++ 06.Entity-Framework-Core/01.ADONET/ConfigClass/ConfigString.cs
@@ -3,6 +3,6 @@
     public class ConfigString
     {
         public const string ConnectionString =
-            @"Data Source = fsc-avconsulting.ch, 1433; Initial Catalog = MinionsDB; User ID = SA; Password = S0ftUn12023@; TrustServerCertificate=True";
+            @"Data Source = xxxxxxxxxxxxxxxx, 1433; Initial Catalog = MinionsDB; User ID = SA; Password = xxxxxxxxxxxxxxxxx; TrustServerCertificate=True";
     }
 }
