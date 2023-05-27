031c843f7d0a08b15e678e15735c9a2fa001b7f7
Krisztian Fodor <kris.fodor66@gmail.com>
2023-05-27 10:19:11 +0200

    wsa-update-dri

---
+++ 12.Windows-Server-Administration/Exam/Info.ps1
@@ -0,0 +1,16 @@
+﻿Write-Host 'Hello and welcome! What might be your name stranger?'
+
+[string] $Name = Read-Host 'Enter your name here'
+
+Write-Host "It is so nice to meet you $Name."
+Write-Host ''
+
+[int] $Age = Read-Host "And what is your age $Name?"
+
+Write-Host 'Oh, my... This is such a wonderful time of your life!'
+
+Write-Host 'My name is Krisztian and I have saved your details in a log file.'
+
+$LogOutput = "{0}, {1}" -f $Name, $Age
+
+$LogOutput | Out-File -FilePath 'C:\Temp\InfoData.log' -Append
---
+++ 12.Windows-Server-Administration/Exam/Track.ps1
@@ -0,0 +1,8 @@
+﻿$cpuCounter = Get-Counter '\Processor(_Total)\% Processor Time' -ErrorAction SilentlyContinue
+    if ($cpuCounter) {
+        $cpuUsage = $cpuCounter.CounterSamples.CookedValue
+        $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        $row = "{0} => {1} %" -f $timestamp, $cpuUsage
+
+        $row | Out-File -FilePath "C:\Temp\TrackData.log" -Append
+    }
