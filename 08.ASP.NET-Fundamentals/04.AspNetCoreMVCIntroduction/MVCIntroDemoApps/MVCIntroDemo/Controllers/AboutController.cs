9daca0dd8cb59f14728a4d0be26d77a4945ccdf9
Krisztian Fodor <kris.fodor66@gmail.com>
2023-05-30 18:14:03 +0200

    just-so-i-feel-like-i-did-something-and-i-dont-cry-alone-in-a-corner

---
+++ 08.ASP.NET-Fundamentals/04.AspNetCoreMVCIntroduction/MVCIntroDemoApps/MVCIntroDemo/Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCIntroDemo.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult IndexAbout()
+        {
+            ViewBag.Message = "This is an ASP.NET Core MVP app.";
+
+            return View();
+        }
+    }
+}
---
+++ 08.ASP.NET-Fundamentals/04.AspNetCoreMVCIntroduction/MVCIntroDemoApps/MVCIntroDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCIntroDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
