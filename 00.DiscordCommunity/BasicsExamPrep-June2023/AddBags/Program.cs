8e9e7f8ede2a59642e2169d9c382cd73c476a2da
Krisztian Fodor <kris.fodor66@gmail.com>
2023-06-09 21:50:36 +0200

    exam-prep-pb-june

---
+++ 00.DiscordCommunity/BasicsExamPrep-June2023/AddBags/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AddBags
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceAbove20 = double.Parse(Console.ReadLine());
+            double kilogramsLuggage = double.Parse(Console.ReadLine());
+            int daysToTrip = int.Parse(Console.ReadLine());
+            int countLuggage = int.Parse(Console.ReadLine());
+
+            double priceBelow10 = 0.2 * priceAbove20;
+            double priceBetween = 0.5 * priceAbove20;
+
+            double totalPrice = 0;
+
+            if (kilogramsLuggage < 10)
+            {
+                totalPrice = priceBelow10 * countLuggage;
+            }
+            else if (kilogramsLuggage >= 10 && kilogramsLuggage <= 20)
+            {
+                totalPrice = priceBetween * countLuggage;
+            }
+            else
+            {
+                totalPrice = priceAbove20 * countLuggage;
+            }
+
+            if (daysToTrip > 30)
+            {
+                totalPrice *= 1.1;
+            }
+            else if (daysToTrip < 7)
+            {
+                totalPrice *= 1.4;
+                //totalPrice = totalPrice * 0.4 + totalPrice
+            }
+            else
+            {
+                totalPrice *= 1.15;
+            }
+
+            Console.WriteLine($"The total price of bags is: {totalPrice:f2} lv.");
+        }
+    }
+}
---
+++ 00.DiscordCommunity/BasicsExamPrep-June2023/Aluminum Joinery/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Aluminum_Joinery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string delivery = Console.ReadLine();
+
+            if (quantity < 10)
+            {
+                Console.WriteLine($"Invalid order");
+                return;
+            }
+
+            double singlePrice = 0;
+            double totalPrice = 0;
+
+            switch (type)
+            {
+                case "90X130":
+                    singlePrice = 110;
+
+                    if (quantity > 30 && quantity <= 60)
+                    {
+                        singlePrice *= 0.95;
+                    }
+                    else if (quantity > 60)
+                    {
+                        singlePrice *= 0.92;
+                    }
+                    break;
+                case "100X150":
+                    singlePrice = 140;
+
+                    if (quantity > 40 && quantity <= 80)
+                    {
+                        singlePrice *= 0.94;
+                    }
+                    else if (quantity > 80)
+                    {
+                        singlePrice *= 0.9;
+                    }
+
+                    break;
+                case "130X180":
+                    singlePrice = 190;
+
+                    if (quantity > 20 && quantity <= 50)
+                    {
+                        singlePrice *= 0.93;
+                    }
+                    else if (quantity > 50)
+                    {
+                        singlePrice *= 0.88;
+                    }
+
+                    break;
+                case "200X300":
+                    singlePrice = 250;
+
+                    if (quantity > 25 && quantity <= 50)
+                    {
+                        singlePrice *= 0.91;
+                    }
+                    else if (quantity > 50)
+                    {
+                        singlePrice *= 0.86;
+                    }
+
+                    break;
+            }
+
+            totalPrice = singlePrice * quantity;
+
+            switch (delivery)
+            {
+                case "With delivery":
+                    totalPrice += 60;
+                    break;
+                default:
+                    break;
+            }
+
+            if (quantity > 99)
+            {
+                totalPrice *= 0.96;
+            }
+
+            Console.WriteLine($"{totalPrice:f2} BGN");
+        }
+    }
+}
